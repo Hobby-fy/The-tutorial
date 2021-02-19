ec8897fbdf619d6c4063223f75f3f2bc16b8e961
Hobby-fy <66298450+Hobby-fy@users.noreply.github.com>
2021-02-19 09:26:40 +0800

    Delete python directory

--- python/2.2.7/2.2.7_read.py
+++
@@ -1,9 +0,0 @@
-from rc522 import RC522
-
-rc = RC522()
-rc.Pcd_start()
-print("Reading...Please place the card...")
-while True:
-    status = rc.read_card_data(2)
-    if status == 0:
-        break
